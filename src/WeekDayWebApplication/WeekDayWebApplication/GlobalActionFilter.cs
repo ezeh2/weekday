09f181d262106cc016aefc3f3a815bfb50a8d369
Edward Reutlinger <e.zeh@gmx.ch>
2025-10-20 14:49:03 +0200

    GlobalActionFilters: warnings eliminated

--- src/WeekDayWebApplication/WeekDayWebApplication/GlobalActionFilter.cs
+++ src/WeekDayWebApplication/WeekDayWebApplication/GlobalActionFilter.cs
@@ -15,14 +15,11 @@ namespace WeekDayWebApplication
         public void OnActionExecuting(ActionExecutingContext context)
         {
             // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-XSS-Protection
-            context.HttpContext.Response.Headers.Add("X-XSS-Protection"
-                , new Microsoft.Extensions.Primitives.StringValues("1"));
+            context.HttpContext.Response.Headers["X-XSS-Protection"] = "1";
             // https://developer.mozilla.org/de/docs/Web/HTTP/Headers/X-Frame-Options
-            context.HttpContext.Response.Headers.Add("X-Frame-Options"
-                , new Microsoft.Extensions.Primitives.StringValues("deny"));
+            context.HttpContext.Response.Headers["X-Frame-Options"] = "deny";
             // https://developer.mozilla.org/de/docs/Web/HTTP/Headers/X-Content-Type-Options
-            context.HttpContext.Response.Headers.Add("X-Content-Type-Options"
-                , new Microsoft.Extensions.Primitives.StringValues("nosniff"));
+            context.HttpContext.Response.Headers["X-Content-Type-Options"] = "nosniff";
 
             // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
             // allow content from:
@@ -31,8 +28,7 @@ namespace WeekDayWebApplication
             //  ajax.aspnetcdn.com
             // 
             // 'unsafe-inline' is needed for <link  asp-fallback-href="..." asp-fallback-test-class="..."  />
-            context.HttpContext.Response.Headers.Add("Content-Security-Policy"
-                , new Microsoft.Extensions.Primitives.StringValues("default-src 'self' stackpath.bootstrapcdn.com ajax.aspnetcdn.com;"));
+            context.HttpContext.Response.Headers["Content-Security-Policy"] = "default-src 'self' stackpath.bootstrapcdn.com ajax.aspnetcdn.com;";
 
             // Strict-Transport-Security: already configured with IApplicationBuilder.UseHsts();
         }
